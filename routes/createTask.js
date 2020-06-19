95345fbd2268de0a90093d5d6433f104066f9994
plaetzaw <plaetzaw@gmail.com>
2020-06-19 17:14:29 -0500

    working to task creation

--- routes/createTask.js
+++ routes/createTask.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
-// let db = require("../models");
-// let bcrypt = require("bcrypt");
+let db = require("../models");
 // const jwt = require("jsonwebtoken");
 
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -11,5 +10,23 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.get("/createTask", (req, res) => {
   res.send("createTask");
 });
+
+router.post(
+  "/createTask",
+  (req,
+  (res) => {
+    let user = req.user.id;
+    let taskTitle = req.body.taskTitle;
+    let taskDescription = req.body.taskDescription;
+    let createdAt = req.body.createdAt;
+
+    let task = db.tasks.build({
+      userId: user,
+      taskTitle: taskTitle,
+      taskDescription: taskDescription,
+      createdAt: createdAt,
+    });
+  })
+);
 
 module.exports = router;
